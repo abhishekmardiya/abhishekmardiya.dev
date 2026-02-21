ab66274787a67f2461ed61ed23da2517d3b555df
Abhishek Mardiya <mardiyaabhishek@gmail.com>
2026-02-21 22:36:41 +0530

    Update LinkedIn URL in SITE_CONSTANTS to use the correct domain format.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,6 +5,6 @@ export const SITE_CONSTANTS = {
       : "https://abhishekmardiya.dev",
   siteName: "Abhishek Mardiya",
   profession: "Software Developer Engineer",
-  linkedinUrl: "https://in/abhishek-mardiya-2a82a3147",
+  linkedinUrl: "https://linkedin.com/in/abhishek-mardiya-2a82a3147",
   githubUrl: "https://github.com/abhishekmardiya",
 };
