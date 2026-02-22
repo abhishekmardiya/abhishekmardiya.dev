fdcce2d565ad53fd870c9a1862b059e0c980ee91
Abhishek Mardiya <mardiyaabhishek@gmail.com>
2026-02-22 21:33:54 +0530

    Add @vercel/analytics dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/component/Footer";
 import { Header } from "@/component/Header";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <div className="min-h-screen flex flex-col justify-between pb-6 pl-[max(1rem,env(safe-area-inset-left))] pr-[max(1rem,env(safe-area-inset-right))] pt-24 sm:pt-20 sm:pl-[max(1.5rem,env(safe-area-inset-left))] sm:pr-[max(1.5rem,env(safe-area-inset-right))] bg-zinc-950 text-zinc-200">
           <main className="max-w-[80ch] mx-auto w-full space-y-6">
             {children}
+            <Analytics />
           </main>
           <Footer />
         </div>
